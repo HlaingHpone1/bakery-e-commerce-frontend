7a3d8c37526f48d9ee002e55f4089a7a9ac8ded1
HlaingHpone1 <hlainghpone1@gmail.com>
2024-08-08 17:03:31 +0630

    Add Validation in Confirm Password

--- src/validation/RegisterValidationSchema.ts
+++ src/validation/RegisterValidationSchema.ts
@@ -4,5 +4,7 @@ export const RegisterValidationSchema = Yup.object().shape({
   username: Yup.string().required("User Name Required"),
   email: Yup.string().email().required("Email Required"),
   password: Yup.string().required("Password Required"),
-  confirm_password: Yup.string().required("Confirm Password Required"),
+  confirm_password: Yup.string()
+    .oneOf([Yup.ref("password")], "Password must be Match")
+    .required("Confirm Password Required"),
 });
